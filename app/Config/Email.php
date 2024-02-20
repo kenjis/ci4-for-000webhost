6042f1f01f930787832045a95e2fae4842ae2a17
kenjis <kenji.uui@gmail.com>
2024-02-21 05:38:49 +0900

    chore: php spark shield:setup

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'admin@example.jp';
+    public string $fromName   = 'Site Admin';
     public string $recipients = '';
 
     /**
