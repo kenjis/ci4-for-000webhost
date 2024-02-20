63689a41415f38eee80657802e9d63025d0bd265
kenjis <kenji.uui@gmail.com>
2024-02-20 14:28:30 +0900

    config: set baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://kenjis.000webhostapp.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
